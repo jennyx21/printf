6f4dd0c542dcc3641c32e37474945866114002f2
jennyx21 <jtruckse@student.42heilbronn.de>
2025-11-20 18:41:50 +0100

    some changes in the print number function, not really far but the next days will work it out

--- ft_printnumbers.c
+++ ft_printnumbers.c
@@ -6,7 +6,7 @@
 /*   By: jtruckse <jtruckse@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 18:19:25 by jtruckse          #+#    #+#             */
-/*   Updated: 2025/11/19 18:23:55 by jtruckse         ###   ########.fr       */
+/*   Updated: 2025/11/20 18:12:37 by jtruckse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 int	ft_putnbr(va_list ap, char type)
 {
+	long int	nr;
+	int			i ;
+
+	i = 0;
+	nr = va_arg(ap, int);
+	while (nr[i])
+	{
+		
+
+	}
 
 }
 
-int ft_putunbr(va_list ap, char type)
+int	ft_putunbr(va_list ap, char type)
 {
-
 }
